60c0f789168fae32cb882715568845467ec2cb2a
x1a0t <thorseraq@gmail.com>
2023-08-21 14:37:45 +0800

    fix: subdoc migration

--- packages/doc-migration/src/index.ts
+++ packages/doc-migration/src/index.ts
@@ -7,51 +7,21 @@ import { Schema } from '@blocksuite/store';
 import { tryMigrate } from '@blocksuite/store-0.6.0';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
-import type { Doc, Map as YMap } from 'yjs';
 
 import { loadYDoc, saveFile as saveYDocJSON, saveYDocBinary } from './util';
 
-const folder = 'asset-simple';
+const isBeforeVersion0_6_0 = false;
+const folder = 'asset-edgeless-new';
 const originalDocFile = `${folder}/doc.bin`;
 const jsonOutputFile = `${folder}/migrated.json`;
 
 const path = resolve(dirname(fileURLToPath(import.meta.url)), originalDocFile);
 const originalDoc = loadYDoc(path);
 
-function trimWhiteboardElements(doc: Doc) {
-  const yMaps: YMap<unknown>[] = [];
-  for (const key in doc.toJSON()) {
-    const map = doc.getMap(key);
-    const keys = [...map.keys()];
-    if (!(keys.includes('versions') || keys.length === 0)) {
-      yMaps.push(map);
-    }
-  }
-  yMaps.forEach(yMap => dealYMap(yMap));
+if (isBeforeVersion0_6_0) {
+  tryMigrate(originalDoc); // migrate step1, forbid, only useful for older doc version
 }
-
-function dealYMap(map: YMap<unknown>) {
-  for (const key of map.keys()) {
-    const block = map.get(key) as YMap<unknown>;
-    if (!block) {
-      console.warn('block empty');
-      continue;
-    }
-    const blockJSON = block.toJSON();
-    if (['affine:surface'].includes(blockJSON['sys:flavour'])) {
-      const elementsMap = block.get('elements') as YMap<unknown> | undefined;
-      if (elementsMap) {
-        elementsMap.clear();
-      } else {
-        console.warn('elements undefined!');
-      }
-    }
-  }
-}
-
-trimWhiteboardElements(originalDoc); // migrate step1
-tryMigrate(originalDoc); // migrate step2
-const migratedDoc = migrateToSubdoc(originalDoc); // migrate step3
+const migratedDoc = migrateToSubdoc(originalDoc); // migrate step2
 
 const intermediateBlockVersions = {
   'affine:code': 1,
@@ -69,24 +39,7 @@ const globalBlockSuiteSchema = new Schema();
 globalBlockSuiteSchema.register(AffineSchemas).register(__unstableSchemas);
 globalBlockSuiteSchema.upgradeWorkspace(migratedDoc);
 for (const subdoc of [...migratedDoc.subdocs]) {
-  globalBlockSuiteSchema.upgradePage(intermediateBlockVersions, subdoc); // migrate step4
-}
-
-// migrate step5
-const subDocs = migratedDoc.get('spaces') as YMap<Doc>;
-for (const key of subDocs.keys()) {
-  const subDoc = subDocs.get(key) as Doc;
-  const blocks = subDoc.get('blocks') as YMap<unknown>;
-
-  for (const key of blocks.keys()) {
-    const block = blocks.get(key) as YMap<unknown>;
-    const flavour = block.get('sys:flavour') as string;
-    if (flavour === 'affine:note') {
-      console.warn('update affine:note!');
-      block.set('prop:index', 'a0');
-      block.set('prop:hidden', false);
-    }
-  }
+  globalBlockSuiteSchema.upgradePage(intermediateBlockVersions, subdoc); // migrate step3
 }
 
 saveYDocJSON(jsonOutputFile, migratedDoc);
